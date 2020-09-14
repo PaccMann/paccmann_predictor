bb76338bf8588d611319f5187886bf76e9526092
Jannis Born <jab@zurich.ibm.com>
2020-09-14 13:33:29 +0200

    doc: Automatic version fetching from __init__

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 """Install package."""
-from setuptools import setup, find_packages
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
+
 
 setup(
     name='paccmann_predictor',
-    version='0.0.1',
+    version=get_version('paccmann_predictor/__init__.py'),
     description=('PyTorch implementation of PaccMann'),
     long_description=open('README.md').read(),
     url='https://github.com/PaccMann/paccmann_predictor',
